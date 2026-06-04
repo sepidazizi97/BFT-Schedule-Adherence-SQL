5c9e43abfa6d09f3a16bf3d9c584a5bfee1d7a0e
Sepideh Azizi <sepid.azizi97@gmail.com>
2026-06-04 12:04:55 -0700

    Create Revenue_Mile_Route.sql

---
+++ Revenue_Mile_Route.sql
@@ -0,0 +1,64 @@
+SELECT DISTINCT TOP 20 
+   "New View"."Month" AS C2,
+   "New View"."Route Short Name" AS C4,
+   "New View"."Direction" AS C6,
+   "New View"."Revenue Miles" AS C8,
+   "New View"."Trip Count" AS C10
+FROM (
+   SELECT
+    dd.Month AS "Month Number",
+    dd.MonthName AS "Month",
+
+    r.RouteShortName AS "Route Short Name",
+    r.RouteName AS "Route Name",
+
+    d.DirectionName AS "Direction",
+
+    SUM(wic.OdometerEnd - wic.OdometerStart) AS "Revenue Miles",
+
+    COUNT(*) AS "Trip Count"
+
+FROM DateDimension dd
+
+INNER JOIN sch_WorkItem wi
+    ON wi.ScheduledDateKey = dd.DateDimensionKey
+
+INNER JOIN sch_WorkItemCompleted wic
+    ON wic.WorkItemKey = wi.WorkItemKey
+
+INNER JOIN sch_Route r
+    ON r.RouteKey = wi.RouteKey
+
+LEFT JOIN sch_Pattern pa
+    ON pa.PatternKey = wi.PatternKey
+
+LEFT JOIN sch_Direction d
+    ON d.DirectionKey = pa.DirectionKey
+
+INNER JOIN BlockItemType blit
+    ON blit.BlockItemTypeKey = wi.BlockItemTypeKey
+
+WHERE
+    dd.FullDate >= CAST('2026-01-01' AS DATETIME)
+    AND dd.FullDate < CAST('2027-01-01' AS DATETIME)
+
+    AND blit.BlockItemTypeCode = 2
+
+    AND r.RouteShortName IN (
+        '1','10','123','123s','170','2','20','225','240',
+        '25','26','26s','27','3','40','41','42',
+        '47','48','50','64','65','67','68'
+    )
+
+    AND d.DirectionName IN ('E','W','N','S')
+
+    AND wic.OdometerEnd > wic.OdometerStart
+
+GROUP BY
+    dd.Month,
+    dd.MonthName,
+    r.RouteShortName,
+    r.RouteName,
+    d.DirectionName
+
+) AS "New View"
